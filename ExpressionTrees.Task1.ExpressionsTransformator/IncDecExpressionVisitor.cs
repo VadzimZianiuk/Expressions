0977e1fc796267774bc173ef0ca9bbee101469f9
Vadzim Zianiuk <Vadzim_Zianiuk@epam.com>
2022-07-17 13:00:39 +0200

    added base task

--- ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
+++ ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -1,9 +1,70 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
     public class IncDecExpressionVisitor : ExpressionVisitor
     {
-        // todo: feel free to add your code here
+        private readonly IReadOnlyDictionary<string, int> _parameters;
+
+        public IncDecExpressionVisitor(IReadOnlyDictionary<string, int> parameters)
+        {
+            _parameters = parameters ?? throw new ArgumentNullException(nameof(parameters));
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            if (_parameters.TryGetValue(node.Name, out var value))
+            {
+                return Expression.Constant(value);
+            }
+
+            return node;
+        }
+
+        protected override Expression VisitLambda<T>(Expression<T> node)
+        {
+            return Expression.Lambda(
+                Visit(node.Body), 
+                node.Parameters.Where(x => !_parameters.ContainsKey(x.Name)));
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (IsIncrementOrDecrement(node.Right) && IsAddOrSubtract(node))
+            {
+                switch (node.Left)
+                {
+                    case ParameterExpression _:
+                    case MemberExpression _:
+                        return IncrementDecrement(node.Left);
+
+                    case BinaryExpression left when IsAddOrSubtract(left):
+                        var right = IncrementDecrement(left.Right);
+                        return left.NodeType == ExpressionType.Add
+                            ? Expression.Add(Visit(left.Left), right)
+                            : Expression.Subtract(Visit(left.Left), right);
+                    default:
+                        break;
+                }
+            }
+
+            return base.VisitBinary(node);
+
+            UnaryExpression IncrementDecrement(Expression expression) =>
+                node.NodeType == ExpressionType.Add
+                    ? Expression.Increment(Visit(expression))
+                    : Expression.Decrement(Visit(expression));
+        }
+
+        private static bool IsAddOrSubtract(BinaryExpression node) =>
+            node.NodeType == ExpressionType.Add || node.NodeType == ExpressionType.Subtract;
+
+        private static bool IsIncrementOrDecrement(Expression node) =>
+            node is ConstantExpression exp 
+            && byte.TryParse(exp.Value.ToString(), out var value) 
+            && value == 1;
     }
 }
--- ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
+++ ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
@@ -7,6 +7,8 @@
  * The results could be printed in console or checked via Debugger using any Visualizer.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
@@ -17,9 +19,45 @@ namespace ExpressionTrees.Task1.ExpressionsTransformer
             Console.WriteLine("Expression Visitor for increment/decrement.");
             Console.WriteLine();
 
-            // todo: feel free to add your code here
+            var expressions = new List<Expression>
+            {
+                (Expression<Func<int, int>>)((int x) => x + 1 + x - 1 * x / x - 1),
+                (Expression<Func<int, int, int>>)((int x, int y) => x + 1 * y - 1 + x - y),
+                (Expression<Func<int, int, int, int>>)((int x, int y, int z) => x + 1 * y - 1 + z + 1),
+            };
+
+            var incDecVisitor = new IncDecExpressionVisitor(GetParams());
+            foreach (var expression in expressions)
+            {
+                var expressionMod = incDecVisitor.Visit(expression);
+                Log(expression, expressionMod);
+            }
 
             Console.ReadLine();
+        }
+
+        private static Dictionary<string, int> GetParams()
+        {
+            Console.WriteLine("Parameters");
+            var parameters = new Dictionary<string, int>
+            {
+                { "a", 99 },
+                { "x", 5 },
+                { "y", 10 },
+            };
+            foreach (var item in parameters)
+            {
+                Console.WriteLine(" {0}: {1}", item.Key, item.Value);
+            }
+
+            return parameters;
+        }
+
+        static void Log(Expression expression, Expression expressionMod)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Source: {0}", expression);
+            Console.WriteLine("   Mod: {0}", expressionMod);
         }
     }
 }
